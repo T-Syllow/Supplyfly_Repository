cf71b4ff5df914794196647c0c6d5c3dbaeb5fec
Phili <Phili@DESKTOP-HSNLGT7.speedport.ip>
2022-05-09 18:01:44 +0200

    hallo

--- SupplyFly/src/supplyfly/objects/Bestellung.java
+++ SupplyFly/src/supplyfly/objects/Bestellung.java
@@ -8,6 +8,8 @@ public class Bestellung {
 	String name;
 	String datum;
 	
+	String testPhilipp;
+	
 	public Bestellung(int bestellnummer, String bestellart, double bestellwert, String name, String datum) {
 		bestellnummer = this.bestellnummer;
 		bestellart = this.bestellart;
