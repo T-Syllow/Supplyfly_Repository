1d10f71b6be1b2d59aae6759498016a635b7bf5a
Tommy Syllow <tommysyllow@gmail.com>
2022-05-23 14:39:09 +0200

    LieferantenProdukteHinzufuegenGUI.java aktualisiert.

--- SupplyFly/src/supplyfly/gui/LieferantenProdukteHinzufuegenGUI.java
+++ SupplyFly/src/supplyfly/gui/LieferantenProdukteHinzufuegenGUI.java
@@ -17,15 +17,8 @@ import java.awt.event.ActionEvent;
 
 public class LieferantenProdukteHinzufuegenGUI {
 
-	private JFrame frame;
-	private JTable table_produktspezifikation;
-	private JTextField textField;
-	private JTextField textField_1;
-	/**
-	 * @wbp.nonvisual location=-30,-29
-	 */
-	private final JTextField textField_2 = new JTextField();
-	private JTextField textField_3;
+	private JFrame frmLieferantenprodukteVerwalten;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -35,7 +28,7 @@ public class LieferantenProdukteHinzufuegenGUI {
 			public void run() {
 				try {
 					LieferantenProdukteHinzufuegenGUI window = new LieferantenProdukteHinzufuegenGUI();
-					window.frame.setVisible(true);
+					window.frmLieferantenprodukteVerwalten.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -54,99 +47,49 @@ public class LieferantenProdukteHinzufuegenGUI {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		textField_2.setColumns(10);
-		frame = new JFrame();
-		frame.setBounds(100, 100, 633, 410);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmLieferantenprodukteVerwalten = new JFrame();
+		frmLieferantenprodukteVerwalten.setTitle("Lieferantenprodukte verwalten");
+		frmLieferantenprodukteVerwalten.setBounds(100, 100, 633, 410);
+		frmLieferantenprodukteVerwalten.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.NORTH);
+		frmLieferantenprodukteVerwalten.getContentPane().add(panel, BorderLayout.NORTH);
 		
-		JLabel lbl_produktname = new JLabel("Produktname:");
+		table = new JTable();
 		
-		JLabel lbl_artikelnummer = new JLabel("Artikelnummer:");
+		JButton btn_speichern = new JButton("Speichern");
 		
-		JLabel lbl_produktpreis = new JLabel("Produktpreis:");
+		JButton btn_abwaehlen = new JButton("Abwählen");
 		
-		JLabel lbl_produktspezifikation = new JLabel("Produktspezifikation:");
-		
-		table_produktspezifikation = new JTable();
-		
-		textField = new JTextField();
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		
-		JButton btn_produkthinzufuegen = new JButton("Produkt hinzuf\u00fcgen");
-		
-		JButton btn_zurueck_produkte = new JButton("Zur\u00fcck");
-		btn_zurueck_produkte.addActionListener(e -> {
-			BestellungenUerbersichtGUI gui = new BestellungenUerbersichtGUI();
-			frame.setVisible(false);
-		});
+		JButton btn_auswaehlen = new JButton("Auswählen");
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
+					.addContainerGap(293, Short.MAX_VALUE)
+					.addComponent(btn_auswaehlen)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btn_abwaehlen)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btn_speichern)
+					.addContainerGap())
 				.addGroup(gl_panel.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel.createParallelGroup(Alignment.LEADING, false)
-							.addGroup(gl_panel.createSequentialGroup()
-								.addComponent(lbl_produktname)
-								.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-								.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-							.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
-								.addComponent(lbl_produktpreis)
-								.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-								.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-							.addGroup(gl_panel.createSequentialGroup()
-								.addComponent(lbl_artikelnummer)
-								.addGap(18)
-								.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
-						.addGroup(gl_panel.createSequentialGroup()
-							.addGap(23)
-							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-								.addComponent(lbl_produktspezifikation)
-								.addGroup(gl_panel.createSequentialGroup()
-									.addComponent(table_produktspezifikation, GroupLayout.PREFERRED_SIZE, 301, GroupLayout.PREFERRED_SIZE)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(btn_zurueck_produkte)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(btn_produkthinzufuegen)))))
-					.addContainerGap(35, Short.MAX_VALUE))
+					.addComponent(table, GroupLayout.PREFERRED_SIZE, 619, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(8, Short.MAX_VALUE))
 		);
 		gl_panel.setVerticalGroup(
-			gl_panel.createParallelGroup(Alignment.TRAILING)
-				.addGroup(Alignment.LEADING, gl_panel.createSequentialGroup()
-					.addGap(32)
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(table, GroupLayout.DEFAULT_SIZE, 328, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lbl_produktname)
-						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(18)
-					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lbl_artikelnummer)
-						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(13)
-					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
-						.addComponent(lbl_produktpreis)
-						.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(29)
-					.addComponent(lbl_produktspezifikation)
-					.addGap(18)
-					.addComponent(table_produktspezifikation, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(35, Short.MAX_VALUE))
-				.addGroup(gl_panel.createSequentialGroup()
-					.addContainerGap(347, Short.MAX_VALUE)
-					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btn_produkthinzufuegen)
-						.addComponent(btn_zurueck_produkte))
+						.addComponent(btn_speichern)
+						.addComponent(btn_abwaehlen)
+						.addComponent(btn_auswaehlen))
 					.addContainerGap())
 		);
 		panel.setLayout(gl_panel);
 	}
-
 }
