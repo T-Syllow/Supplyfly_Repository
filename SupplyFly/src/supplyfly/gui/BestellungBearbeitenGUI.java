0329a2fff10c7a771d831a34fb9c4d475d2343de
Tommy Syllow <tommysyllow@gmail.com>
2022-06-08 12:02:58 +0200

    BestellungBearbeitenGUI.java GUI neu angeordnet. Button bearbeiten del.

--- SupplyFly/src/supplyfly/gui/BestellungBearbeitenGUI.java
+++ SupplyFly/src/supplyfly/gui/BestellungBearbeitenGUI.java
@@ -17,6 +17,7 @@ import javax.swing.table.DefaultTableModel;
 import supplyfly.datenbankzugriff.DBAccess;
 
 import javax.swing.JScrollPane;
+import java.awt.Font;
 
 public class BestellungBearbeitenGUI {
 
@@ -52,7 +53,7 @@ public class BestellungBearbeitenGUI {
 	 */
 	private void initialize(String bestellNr) {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 600, 500);
+		frame.setBounds(100, 100, 792, 527);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
@@ -61,12 +62,16 @@ public class BestellungBearbeitenGUI {
 		JScrollPane scrollPane = new JScrollPane();
 		
 		JLabel lbl_bestellnummer = new JLabel("Bestellnummer:");
+		lbl_bestellnummer.setFont(new Font("Lucida Grande", Font.BOLD, 13));
 		
 		JLabel lbl_bestelltvon = new JLabel("bestellt von:");
+		lbl_bestelltvon.setFont(new Font("Lucida Grande", Font.BOLD, 13));
 		
 		JLabel lbl_status = new JLabel("Status:");
+		lbl_status.setFont(new Font("Lucida Grande", Font.BOLD, 13));
 		
 		JLabel lbl_bestellwert = new JLabel("Bestellwert:");
+		lbl_bestellwert.setFont(new Font("Lucida Grande", Font.BOLD, 13));
 		
 		JLabel lbl_targetBestellnummer = new JLabel("[EMPTY]");
 		
@@ -75,8 +80,6 @@ public class BestellungBearbeitenGUI {
 		JLabel lbl_targetStatus = new JLabel("[EMPTY]");
 		
 		JLabel lbl_targetBestellwert = new JLabel("[EMPTY]");
-		
-		JButton btn_bearbeiten = new JButton("Bearbeiten");
 		
 		JButton btn_zurueck = new JButton("Zurueck");
 		
@@ -87,62 +90,62 @@ public class BestellungBearbeitenGUI {
 				.addGroup(gl_panel.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-							.addGroup(gl_panel.createSequentialGroup()
-								.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 311, GroupLayout.PREFERRED_SIZE)
-								.addGap(18)
-								.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-									.addComponent(lbl_bestellnummer)
-									.addComponent(lbl_bestelltvon)
-									.addComponent(lbl_status)
-									.addComponent(lbl_bestellwert))
-								.addPreferredGap(ComponentPlacement.RELATED, 57, Short.MAX_VALUE)
-								.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-									.addComponent(lbl_targetBestellnummer)
-									.addComponent(lbl_targetBestelltvon)
-									.addComponent(lbl_targetStatus)
-									.addComponent(lbl_targetBestellwert))
-								.addContainerGap(50, Short.MAX_VALUE))
-							.addGroup(gl_panel.createSequentialGroup()
-								.addComponent(btn_bearbeiten)
-								.addContainerGap(477, Short.MAX_VALUE)))
-						.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
-							.addComponent(btn_bestaetigen)
+						.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 791, Short.MAX_VALUE)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+								.addGroup(Alignment.LEADING, gl_panel.createSequentialGroup()
+									.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+										.addGroup(gl_panel.createSequentialGroup()
+											.addComponent(lbl_bestelltvon)
+											.addPreferredGap(ComponentPlacement.RELATED, 195, Short.MAX_VALUE)
+											.addComponent(lbl_targetBestelltvon))
+										.addGroup(gl_panel.createSequentialGroup()
+											.addComponent(lbl_bestellnummer)
+											.addGap(176)
+											.addComponent(lbl_targetBestellnummer))
+										.addGroup(gl_panel.createSequentialGroup()
+											.addComponent(lbl_status)
+											.addPreferredGap(ComponentPlacement.RELATED, 230, Short.MAX_VALUE)
+											.addComponent(lbl_targetStatus)))
+									.addGap(366))
+								.addGroup(gl_panel.createSequentialGroup()
+									.addComponent(lbl_bestellwert)
+									.addGap(53)
+									.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+										.addComponent(lbl_targetBestellwert)
+										.addComponent(btn_bestaetigen))
+									.addGap(6)))
 							.addPreferredGap(ComponentPlacement.RELATED)
 							.addComponent(btn_zurueck)
-							.addContainerGap())))
+							.addGap(8)))
+					.addContainerGap())
 		);
 		gl_panel.setVerticalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel.createSequentialGroup()
-					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 356, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(btn_bearbeiten))
-						.addGroup(gl_panel.createSequentialGroup()
-							.addGap(41)
-							.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lbl_bestellnummer)
-								.addComponent(lbl_targetBestellnummer))
-							.addGap(33)
-							.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lbl_bestelltvon)
-								.addComponent(lbl_targetBestelltvon))
-							.addGap(37)
-							.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lbl_status)
-								.addComponent(lbl_targetStatus))
-							.addPreferredGap(ComponentPlacement.RELATED, 175, Short.MAX_VALUE)
-							.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lbl_bestellwert)
-								.addComponent(lbl_targetBestellwert))
-							.addGap(87)
-							.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-								.addComponent(btn_zurueck)
-								.addComponent(btn_bestaetigen))))
-					.addContainerGap())
+					.addContainerGap()
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 109, GroupLayout.PREFERRED_SIZE)
+					.addGap(28)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lbl_bestellnummer)
+						.addComponent(lbl_targetBestellnummer))
+					.addGap(28)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lbl_bestelltvon)
+						.addComponent(lbl_targetBestelltvon))
+					.addGap(26)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lbl_status)
+						.addComponent(lbl_targetStatus))
+					.addPreferredGap(ComponentPlacement.RELATED, 69, Short.MAX_VALUE)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lbl_bestellwert)
+						.addComponent(lbl_targetBestellwert))
+					.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btn_zurueck)
+						.addComponent(btn_bestaetigen))
+					.addGap(16))
 		);
 		
 		BestellungenUebersicht = new JTable();
@@ -154,7 +157,7 @@ public class BestellungBearbeitenGUI {
 				}
 			) {
 				boolean[] columnEditables = new boolean[] {
-					false, false, false, false, false, false, false
+					true, true, true, true, true, true, true
 				};
 				public boolean isCellEditable(int row, int column) {
 					return columnEditables[column];
